1ab2af873d935580f380b28a5e194839df4d4e19
hperez29 <perezherbie29@yahoo.com>
2025-05-12 06:17:40 -0400

    Userinterface

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,55 @@
 package com.pluralsight;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+    private Dealership dealership;
+    private Scanner scanner;
+
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void display() {
+        init();
+        boolean exit = false;
+        while (!exit) {
+            displayMenu();
+            String command = scanner.nextLine();
+            switch (command) {
+                case "1":
+                    processAllVehiclesRequest();
+                    break;
+                case "0":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private void init() {
+        DealershipFileManager fileManager = new DealershipFileManager();
+        dealership = fileManager.getDealership();
+    }
+
+    private void displayMenu() {
+        System.out.println("Dealership Inventory System");
+        System.out.println("1 - Display All Vehicles");
+        System.out.println("0 - Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    private void displayVehicles(List<Vehicle> vehicles) {
+        for (Vehicle vehicle : vehicles) {
+            System.out.println(vehicle);
+        }
+    }
+
+    private void processAllVehiclesRequest() {
+        List<Vehicle> vehicles = dealership.getAllVehicles();
+        displayVehicles(vehicles);
+    }
 }
